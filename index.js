a089cd8c8d19b174e7fa91d83d418f544f918884
Varga Attila Tibor <h053414@stud.u-szeged.hu>
2023-03-25 23:08:16 +0100

    #2 Create canvas and start writing the code. Make gravity logic

---
+++ index.js
@@ -0,0 +1,44 @@
+const canvas = document.querySelector('canvas')
+const c = canvas.getContext('2d')
+
+canvas.width = 1024
+canvas.height = 576
+
+const gravity = 0.5
+
+class Player {
+	constructor(position){
+		this.position = position
+		this.velocity = {x:0,y:1}
+		this.height = 100
+	}
+
+	draw() {
+		c.fillStyle = 'red'
+		c.fillRect(this.position.x, this.position.y, 100, this.height)
+	}
+
+	update() {
+		this.draw()
+		this.position.y += this.velocity.y
+		
+		// If the player reaches the bottom of the canvas, the velocity value will be set to 0 (it will stop falling)
+		if (this.position.y + this.height + this.velocity.y < canvas.height)
+			this.velocity.y += gravity
+		else this.velocity.y = 0
+	}
+}
+
+const player = new Player({x:0,y:0})
+
+let y = 100
+function animate() {
+	window.requestAnimationFrame(animate)
+	c.fillStyle = 'white'
+	c.fillRect(0, 0, canvas.width, canvas.height)
+	player.update()
+	
+	
+}
+
+animate()
